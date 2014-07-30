8748c58a55bf225bc12e26a01fb4d7640dd78613
SirMaggot <SirMaggot@users.noreply.github.com>
2014-07-30 18:37:05 +0200

    Create Avfuktare.lua
    Lua för att styra en avfuktare

---
+++ Avfuktare.lua
@@ -0,0 +1,27 @@
+--[[
+%% autostart
+%% properties
+19 ui.Label1.value
+%% globals
+--]]
+--19 value = ID of humidity sensor
+
+--Beskrivning
+--Om luftfuktighet är över 66.0%, Starta avfuktare
+--Om luftfuktighet är under 64.0%, Stoppa avfuktare
+        
+	local humidity = fibaro:getValue(19, "ui.Label1.value")
+	local humStart = "66" --Starta avfuktare om över %
+	local humStopp = "64" --Stoppa avfuktaren om under %
+	local avfuktare = 13 --Min Powerplug
+	
+		--fibaro:debug("Check of humidity, right now: "..humidity.."%.")
+	if (humidity > humStart) then --Om luftfuktighet är över 66, starta avfuktare
+      fibaro:call(avfuktare, "turnOn") --Starta avfuktare
+		fibaro:sleep(300*1000)--Pausa i 5 minuter
+	end
+             
+	if (humidity < humStopp) then --Om luftfuktighet är under 64, stoppa avfuktare
+	fibaro:call(avfuktare, "turnOff") --Stoppa av avfuktare
+		fibaro:sleep(300*1000)--Pausa i 5 minuter
+	end
